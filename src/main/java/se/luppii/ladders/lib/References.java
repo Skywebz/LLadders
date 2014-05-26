22fbe3bf19910722f97132f93dcf54734f4b032c
Calle Lejdbrandt <aiqueneldar@gmail.com>
2014-05-26 03:02:33 +0200

    Update mod version to 0.3
    With new function to ladders version is now 0.3.

--- src/main/java/se/luppii/ladders/lib/References.java
+++ src/main/java/se/luppii/ladders/lib/References.java
@@ -5,7 +5,7 @@ public class References {
 	public static final String MOD_ID = "LLadders";
 	public static final String MOD_NAME = "Luppii's Ladders";
 	public static final String MOD_STRING = "lladders.";
-	public static final String MOD_VERSION = "0.2.1";
+	public static final String MOD_VERSION = "0.3";
 	public static final String CLIENT_PROXY_LOCATION = "se.luppii.ladders.proxy.ClientProxy";
 	public static final String COMMON_PROXY_LOCATION = "se.luppii.ladders.proxy.CommonProxy";
 }
