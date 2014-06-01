2773bba4cfd1bf03e56de76cf0d9eac58f35ad73
Calle Lejdbrandt <aiqueneldar@gmail.com>
2014-06-01 09:05:14 +0200

    Cleaning and documentation for issue #14
    Done som cleaning and sepparation of code into a superclass to ease
    maintenence. Also documented some code for future use. This also fixes
    issue #14

--- src/main/java/se/luppii/ladders/block/BlockRopeLadder.java
+++ src/main/java/se/luppii/ladders/block/BlockRopeLadder.java
@@ -31,7 +31,7 @@ import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class BlockRopeLadder extends Block implements ITileEntityProvider {
+public class BlockRopeLadder extends BlockGenericLadder implements ITileEntityProvider {
 
 	private IIcon blockIIcon;
 	private boolean enableLeftClick;
@@ -45,57 +45,6 @@ public class BlockRopeLadder extends Block implements ITileEntityProvider {
 		this.setBlockName("lladders.block.ropeladder");
 		this.setCreativeTab(CreativeTabs.tabDecorations);
 		enableLeftClick = par1Boolean;
-	}
-	
-	public boolean isBlockNormalCube(World world, int i, int j, int k) {
-		return false;
-	}
-	
-	public AxisAlignedBB getCollisionBoundingBoxFromPool(World par1World, int par2, int par3, int par4) {		
-		this.setBlockBoundsBasedOnState(par1World, par2, par3, par4);
-		//Fix a collisionbox that is tiny bit bigger than the block the ladder is placed on. This to enable climbing from both sides
-		int direction = par1World.getBlockMetadata(par2, par3, par4) & 3;
-		float factor = 1.0F;
-	    float f = factor / 16.0F;
-	    
-	    switch (direction) {
-	    case 0:
-	    	//SOUTH
-	    	AxisAlignedBB box = super.getCollisionBoundingBoxFromPool(par1World, par2, par3, par4);
-	    	box = box.expand(0, 0, -f);
-	    	return box;
-	    	
-	    case 1:
-	    	//WEST
-	    	box = super.getCollisionBoundingBoxFromPool(par1World, par2, par3, par4);
-	    	box = box.expand(-f, 0, 0);
-	    	return box;
-	    		    
-	    case 2:
-	    	//NORTH
-	    	box = super.getCollisionBoundingBoxFromPool(par1World, par2, par3, par4);
-	    	box = box.expand(0, 0, -f);
-	    	return box;
-	    
-	    case 3:
-	    	//EAST
-	    	box = super.getCollisionBoundingBoxFromPool(par1World, par2, par3, par4);
-	    	box = box.expand(-f, 0, 0);
-	    	return box;
-	    }
-	    
-	    return AxisAlignedBB.getBoundingBox(par2 + f, par3, par4 + f, par2 + factor - f, par3 + factor - f, par4 + factor - f);
-	    
-	}
-	
-	@SideOnly(Side.CLIENT)
-	public AxisAlignedBB getSelectedBoundingBoxFromPool(World par1World, int par2, int par3, int par4) {	
-		this.setBlockBoundsBasedOnState(par1World, par2, par3, par4);
-		return super.getSelectedBoundingBoxFromPool(par1World, par2, par3, par4);
-	}
-	
-	public void setBlockBoundsBasedOnState(IBlockAccess par1IBlockAccess, int par2, int par3, int par4) {	
-		this.updateLadderBounds(par1IBlockAccess.getBlockMetadata(par2, par3, par4));
 	}
 	
     public void updateLadderBounds(int par1) {
@@ -121,89 +70,6 @@ public class BlockRopeLadder extends Block implements ITileEntityProvider {
 		}
 	}
 	
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity) {
-    	//Check to see if it is a player as we are only interested in manipulating them
-    	if (entity instanceof EntityPlayer) {
-    		//Cast to EntityPlayer to make it more strict
-    		EntityPlayer player = (EntityPlayer) entity;
-    		
-    		int direction = world.getBlockMetadata(x, y, z) & 3;
-    		
-    		boolean player_close = false;
-    		
-    		//Look at placement position and calculate if the player is close enough to ladder to "climb"
-    		switch (direction) {
-    		//SOUTH
-    		case 0:
-    			if (player.posZ - z >= 0.8D)
-    				player_close = true;
-    			break;
-    			
-    		//WEST
-    		case 1:
-    			if (player.posX - x <= 0.2D)
-    				player_close = true;
-    			break;
-    			
-    		//NORTH
-    		case 2:
-    			if (player.posZ - z <= 0.2D)
-    				player_close = true;
-    			break;
-    			
-    		//EAST
-    		case 3:
-    			if (player.posX - x >= 0.8D)
-    				player_close = true;
-    			break;
-    		
-    		default:
-    			player_close = false;
-    		}
-    		
-    		if (player.posY - 1.0D >= y && player_close) {
-				player.moveForward = 0.0F;
-				
-				//If player is moving down, move slowly down.
-				if (player.motionY < -0.15D)
-					player.motionY = -0.15D;
-				
-				
-				//check if we want to climbe up
-				if (GameSettings.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindForward) || GameSettings.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindBack) || GameSettings.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindLeft) || GameSettings.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindRight)) {
-					if (player.motionY < 0.2D)
-						player.motionY = 0.2D;
-				}		
-    		}
-    		
-    		//Check if we are sneaking and want to climb up, or just want to sneak "stand still" on the ladder
-    		if (GameSettings.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindSneak) && (GameSettings.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindForward) || GameSettings.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindBack) || GameSettings.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindLeft) || GameSettings.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindRight))) {
-    			player.motionY = 0.2D;
-    		} else if (GameSettings.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindSneak)) {	
-    			player.setVelocity(0.0D, 0.08D, 0.0D); //Found this by experimenting. An upward velocity of 0.08 negates gravity fall
-    		}
-    	}
-    }
-    
-	public boolean isOpaqueCube() {
-		return false;
-	}
-	
-	public boolean renderAsNormalBlock() {	
-		return false;
-	}
-	
-	public static void setRenderID(int id) {		
-		renderID = id;
-	}
-	
-	@Override
-	public int getRenderType() {		
-		return renderID;
-	}
-	
 	public boolean canPlaceBlockAt(World par1World, int par2, int par3, int par4) {
 		
 		return par1World.isSideSolid(par2 - 1, par3, par4, EAST ) ||
@@ -220,10 +86,10 @@ public class BlockRopeLadder extends Block implements ITileEntityProvider {
 		int j1 = par9 & 3;
 		
 		if (par1World.getBlock(par2, par3 + 1, par4) == this) {
-			return par1World.getBlockMetadata(par2, par3 + 1, par4);
+			return par1World.getBlockMetadata(par2, par3 + 1, par4) & 3;
 		}
 		if (par1World.getBlock(par2, par3 - 1, par4) == this) {
-			return par1World.getBlockMetadata(par2, par3 - 1, par4);
+			return par1World.getBlockMetadata(par2, par3 - 1, par4) & 3;
 		}
 		if ((par5 == 1 || par5 == 2) && par1World.isSideSolid(par2, par3, par4 + 1, NORTH)) {
 			
@@ -313,27 +179,6 @@ public class BlockRopeLadder extends Block implements ITileEntityProvider {
 		super.onNeighborBlockChange(par1World, par2, par3, par4, par5Block);
 	}
 	
-	public int quantityDropped(Random par1Random) {
-		return 1;
-	}
-	
-	@Override
-	public boolean isLadder(IBlockAccess world, int x, int y, int z, EntityLivingBase entity) {
-		return true;
-	}
-	
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void registerBlockIcons(IIconRegister par1IIconRegister) {		
-		blockIIcon = par1IIconRegister.registerIcon("lladders:" + getUnlocalizedName());
-	}
-	
-	@Override
-	@SideOnly(Side.CLIENT)
-	public IIcon getIcon(int par1, int par2) {		
-		return blockIIcon;
-	}
-	
 	public TileEntity getBlockEntity(int par1) {		
 		return new TileEntityRopeLadder();
 	}
@@ -364,7 +209,7 @@ public class BlockRopeLadder extends Block implements ITileEntityProvider {
 		return new TileEntityRopeLadder();
 	}
 	
-	private boolean canSetLadder(World world, int x, int y, int z, int meta) {
+	protected boolean canSetLadder(World world, int x, int y, int z, int meta) {
 		
 		if (world.getBlock(x, y, z) == this) {
 			
@@ -377,7 +222,7 @@ public class BlockRopeLadder extends Block implements ITileEntityProvider {
 		return true;
 	}
 	
-	private boolean setLadder(World world, int x, int y, int z, int meta, EntityPlayer player) {
+	protected boolean setLadder(World world, int x, int y, int z, int meta, EntityPlayer player) {
 		
 		Block block = world.getBlock(x, y, z);
 		if (world.isAirBlock(x, y, z)) {
